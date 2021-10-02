bfc430bd168e684619ed293ec31f34a385854181
IvanovMaxim <maxim.ivanov@matryoshka.com>
2021-10-02 15:08:39 +0300

    Main Menu

---
+++ Assets/Resources/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button _startButton;
+
+    private void Awake()
+    {
+        _startButton.onClick.AddListener(OnStartButton);
+    }
+
+    private void OnStartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
