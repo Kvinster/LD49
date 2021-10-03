23b5f992947eb0a783dabcc712f8aa8785958e57
IvanovMaxim <maxim.ivanov@matryoshka.com>
2021-10-03 11:01:17 +0300

    Thorns

---
+++ Assets/Scripts/Behaviour/Thorns.cs
@@ -0,0 +1,16 @@
+﻿using LD49.Manager;
+using UnityEngine;
+
+namespace LD49.Behaviour
+{
+    public class Thorns : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("Player") || other.CompareTag("Bomb"))
+            {
+               LevelManager.Instance.FailLevel();
+            }
+        }
+    }
+}
