e860b86f81c95192b16cb84716330d737d914bcb
IvanovMaxim <maxim.ivanov@matryoshka.com>
2021-10-03 12:03:36 +0300

    Springboard

---
+++ Assets/Scripts/Behaviour/Springboard.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace LD49.Behaviour
+{
+    public class Springboard : MonoBehaviour
+    {
+        [SerializeField]
+        private float _springForce = 1200.0f;
+        
+        private Animator _animator;
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(other.CompareTag("Player") || other.CompareTag("Bomb"))
+            {
+                Tossing(other.gameObject);
+            }
+        }
+        
+        private void Tossing(GameObject gameObject)
+        {
+            _animator.SetTrigger("Tossing");
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _springForce);
+        }
+    }
+}
